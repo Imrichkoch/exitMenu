ba5966d906852f0e82225c7fbdff36f045841703
Imrichkoch <imrichkoch@gmail.com>
2017-09-04 18:39:02 +0200

    oprava na menu

--- menuexit/src/menuexit/MenuExit.java
+++ menuexit/src/menuexit/MenuExit.java
@@ -3,18 +3,22 @@ package menuexit;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-public class MenuExit extends JFrame{
+public class MenuExit extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3324011838043471912L;
-	
+
 	JPanel thePanel = new JPanel();
 
 	public MenuExit() {
@@ -23,27 +27,27 @@ public class MenuExit extends JFrame{
 		this.setVisible(true);
 		this.setTitle("Menu Exit");
 		this.setSize(800, 600);
-		JButton exitButton = new JButton("EXIT");
-		exitButton.addActionListener(l -> System.exit(0));
-		
-		
-		
-		thePanel.add(exitButton);
-		
-		add(thePanel);
-		
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Menu");
+
+		JMenuItem exit = new JMenuItem("Exit");
+		menu.add(exit);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension dim = tk.getScreenSize();
 		int xPos = (dim.width / 2) - (this.getWidth() / 2);
 		int yPos = (dim.height / 2) - (this.getHeight() / 2);
 		this.setLocation(xPos, yPos);
-		
-		
-		
-		
-		
+
 	}
-	
-	
 
 }
